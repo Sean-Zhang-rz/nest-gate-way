0840f9c3cf005ca4e98b823271fb7c47eb0f1396
sean <770899447@qq.com>
2023-06-19 19:42:29 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import {  Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { getConfig } from './utils';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+    }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig]
+    }), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { FeishuService } from './feishu/feishu.service';
+import { FeishuController } from './feishu/feishu.controller';
+
+@Module({
+  controllers: [
+    FeishuController
+  ],
+  providers: [FeishuService],
+})
+export class UserModule { }
